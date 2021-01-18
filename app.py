76cdf8ab1e9fd5c850089daf58ca177a7f4c130e
escape0707 <tothesong@gmail.com>
2021-01-18 21:30:38 +0800

    extract_expressions

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import itertools
 import pathlib
 import shutil
+from typing import List
 
 import pydub
 import pydub.silence
-
 
 working_dir = pathlib.Path("./working")
 output_dir = working_dir / "output"
@@ -19,15 +19,29 @@ word_count = int(word_count_file.read_text())
 def split_and_save(input_file: pathlib.Path, output_dir: pathlib.Path) -> None:
     soundtrack = pydub.AudioSegment.from_file(input_file)
     chuck_collection = pydub.silence.split_on_silence(
-        soundtrack, min_silence_len=800, silence_thresh=-50)
+        soundtrack, min_silence_len=800, silence_thresh=-50
+    )
 
     for i, chunk in enumerate(itertools.islice(chuck_collection, 2, None)):
         chunk.export(output_dir / f"{i}.mp3")
 
 
-def map_mp3_and_word() -> None:
+def extract_expressions(expression_field: str) -> List[str]:
+    expression_collection = []
+    word_start = 0
+    word_end = expression_field.find("（")
+    while True:
+        if word_end == -1:
+            word_end = len(expression_field)
+        expression_collection.append(expression_field[word_start:word_end])
+        word_start = expression_field.find("（", word_end + 1)
+        if word_start == -1:
+            break
+        word_end = expression_field.find("）", word_start)
+    return expression_collection
+
+
+def process_mp3_collection() -> None:
     vocab_list_file = working_dir / "vocab_list.txt"
     with open(vocab_list_file) as f:
-        for line in f:
-            field_collection = line.split("\t")
-            
+        map(pair_mp3_with_word, f)
