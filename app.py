6e1d8a7323d6687213e07b7d2cf0548c07d50662
escape0707 <tothesong@gmail.com>
2021-01-18 21:04:36 +0800

    before starting

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import itertools
 import pathlib
 import shutil
 
 import pydub
 import pydub.silence
+
 
 working_dir = pathlib.Path("./working")
 output_dir = working_dir / "output"
@@ -10,10 +12,22 @@ shutil.rmtree(output_dir)
 output_dir.mkdir()
 input_file = next((working_dir / "input").iterdir())
 
-soundtrack = pydub.AudioSegment.from_file(input_file)
-chucks = pydub.silence.split_on_silence(
-    soundtrack, min_silence_len=700, silence_thresh=-40, keep_silence=200
-)
+word_count_file = working_dir / "processed_word_count_cache.txt"
+word_count = int(word_count_file.read_text())
 
-for i, chunk in enumerate(chucks):
-    chunk.export(output_dir / f"{i}.mp3")
+
+def split_and_save(input_file: pathlib.Path, output_dir: pathlib.Path) -> None:
+    soundtrack = pydub.AudioSegment.from_file(input_file)
+    chuck_collection = pydub.silence.split_on_silence(
+        soundtrack, min_silence_len=800, silence_thresh=-50)
+
+    for i, chunk in enumerate(itertools.islice(chuck_collection, 2, None)):
+        chunk.export(output_dir / f"{i}.mp3")
+
+
+def map_mp3_and_word() -> None:
+    vocab_list_file = working_dir / "vocab_list.txt"
+    with open(vocab_list_file) as f:
+        for line in f:
+            field_collection = line.split("\t")
+            
