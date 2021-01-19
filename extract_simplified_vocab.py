16c1ae61005469c43b4e68449d3e8960c64cebca
escape0707 <tothesong@gmail.com>
2021-01-19 15:36:20 +0800

    Update extract_simplified_vocab

--- extract_simplified_vocab.py
+++ extract_simplified_vocab.py
@@ -1,32 +1,33 @@
-from typing import List
+import re
+from typing import Iterator, List, Tuple
 
 from common import new_vocab_list_file, simplified_vocab_lists_dir
 from utils import extract_expressions
 
 
-def extract_lesson(tag_field: str) -> int:
-    tag_collection = tag_field.split()
-    lesson = -1
-    for tag in tag_collection:
-        if len(tag) == 3:  # should use regex
-            lesson = int(tag[1:])
-    return lesson
+def extract_lesson(tag_field: str) -> Tuple[Iterator[int], Iterator[int]]:
+    lesson_collection = map(int, re.findall(r"L(\d{2})(?!U)", tag_field))
+    lesson_u_collection = map(int, re.findall(r"L(\d{2})U", tag_field))
+    assert lesson_collection or lesson_u_collection
+    return lesson_collection, lesson_u_collection
 
 
-def simplify_vocab() -> List[List[str]]:
+def simplify_vocab() -> Tuple[List[List[str]], List[List[str]]]:
     simplified_collection = [[] for i in range(51)]
+    extra_collection = [[] for i in range(51)]
     with open(new_vocab_list_file) as in_f:
         for note in in_f:
             field_collection = note.split("\t")
             expression_collection = extract_expressions(field_collection[1])
-            lesson = extract_lesson(field_collection[-1])
-            if lesson == -1:
-                continue
-            simplified_collection[lesson - 1].extend(expression_collection)
-    return simplified_collection
+            lesson_collection, lesson_u_collection = extract_lesson(field_collection[-1])
+            for lesson in lesson_collection:
+                simplified_collection[lesson - 1].extend(expression_collection)
+            for lesson_u in lesson_u_collection:
+                extra_collection[lesson_u - 1].extend(expression_collection)
+    return simplified_collection, extra_collection
 
 
-def save_simplified_vocab(simplified_collection: List[List[str]]) -> None:
+def save_simplified_vocab(simplified_collection: List[List[str]], extra_collection: List[List[str]]) -> None:
     if not simplified_vocab_lists_dir.exists():
         simplified_vocab_lists_dir.mkdir()
     for lesson, simplified in enumerate(simplified_collection, 1):
@@ -34,10 +35,15 @@ def save_simplified_vocab(simplified_collection: List[List[str]]) -> None:
             str(lesson).zfill(2) + ".txt"
         )
         simplified_vocab_list_file.write_text("\n".join(simplified) + "\n")
+    for lesson_u, extra in enumerate(extra_collection, 1):
+        simplified_vocab_list_file = simplified_vocab_lists_dir / (
+            str(lesson_u).zfill(2) + "U.txt"
+        )
+        simplified_vocab_list_file.write_text("\n".join(extra) + "\n")
 
 
 def extract_simplified_vocab() -> None:
-    save_simplified_vocab(simplify_vocab())
+    save_simplified_vocab(*simplify_vocab())
 
 
 if __name__ == "__main__":
