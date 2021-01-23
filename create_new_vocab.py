e0940a59668a35e1b85c253081c39f76b5885c06
escape0707 <tothesong@gmail.com>
2021-01-23 17:44:56 +0800

    Revise previous work after removed expression_field

--- create_new_vocab.py
+++ create_new_vocab.py
@@ -7,15 +7,16 @@ def create_new_vocab() -> None:
         for index, note in enumerate(in_f, 1):
             field_collection = note.split("\t")
             tag_field = field_collection.pop()  # end with an redundant "\n"
-            expression_collection = extract_expressions(
-                furigana_to_kanji(field_collection[0])
-            )
+            expression_field = furigana_to_kanji(field_collection[0])
+            expression_collection = extract_expressions(expression_field)
             index_field = str(index)
             audio_field = "".join(
                 map(lambda x: f"[sound:MNN_{x}.mp3]", expression_collection)
             )
             new_note = "\t".join(
-                [index_field] + field_collection + [audio_field, tag_field]
+                [index_field, expression_field]
+                + field_collection
+                + [audio_field, tag_field]
             )
             out_f.write(new_note)
 
